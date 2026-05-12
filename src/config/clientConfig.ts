27b94bd4b2606f1e28d96dd56cb3d9358eb970a2
aioobalharash-beep <aioob.alharash@gmail.com>
2026-05-12 14:12:33 +0400

    Update clientConfig.ts

--- src/config/clientConfig.ts
+++ src/config/clientConfig.ts
@@ -80,7 +80,7 @@ export const CLIENT_CONFIG: ClientConfig = {
     hasEvent: false,
   },
   checkInOut: {
-    checkInTime: '13:00',   // 2:00 PM
+    checkInTime: '13:00',   // 1:00 PM
     checkOutTime: '11:00',  // 11:00 AM (next day for night stays)
   },
 };
